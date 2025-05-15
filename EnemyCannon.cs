2698aaeaee9c5d1dae12dd6b9fde70152a78df64
CongruentBob <joel.cawte@gmail.com>
2025-05-15 09:59:22 +0100

    Part 13

--- EnemyCannon.cs
+++ EnemyCannon.cs
@@ -8,12 +8,16 @@ public partial class EnemyCannon : Node2D
     [Export]
     public Stats Stats { get; set; }
     private Hurtbox _hurtbox;
+    private AnimationPlayer _effectsAnimationPlayer;
+
 
     // Called when the node enters the scene tree for the first time.
+
     public override void _Ready()
 	{
 		_hurtbox = GetNode<Hurtbox>("Hurtbox");
 		_hurtbox.HurtboxEntered += OnHurtboxEntered;
+		_effectsAnimationPlayer = GetNode<AnimationPlayer>("EffectsAnimationPlayer");
 		Stats.NoHealth += OnNoHealth;
 	}
 
@@ -21,6 +25,7 @@ public partial class EnemyCannon : Node2D
     private void OnHurtboxEntered(Hitbox area)
     {
 		Stats.Health -= area.Damage;
+		_effectsAnimationPlayer.Play("hitflash");
     }
 
     private void OnNoHealth()
